0ead149c2a587e56638af19ee779e960167b7a0d
federicogarcik <federico82008@gmail.com>
2017-02-01 11:58:04 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,10 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
+	//Esto es un comentario de una linea
+	var Nombre ;//Defino variable
+	Nombre = prompt("Ingrese su nombre",  "Natalia Natalia"); //Ingreso el nombre del usuario en la variable Nombre; 
+	alert("Nombre:  " + Nombre); //Contatenar: mostrar valores literales con valores de variables (muestro "nombre" y variable Nombre);
 	
 }
 
