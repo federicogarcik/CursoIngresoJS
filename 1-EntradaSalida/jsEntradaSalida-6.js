a1a1653b5c783b389b73b5c7e245fcf1f84b9fd9
federicogarcik <federico82008@gmail.com>
2017-02-07 11:47:13 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,17 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
-
+	var NumeroUno;//Defino variable del primer numero
+	var NumeroDos;//Defino variable del segundo numero
+	var Resultado;//Defino variable de las suma de los numeros uno y dos
+	
+	NumeroUno = document.getElementById('numeroUno').value;//Tomo datos por ID en la variable "Numero1"
+	NumeroDos = document.getElementById('numeroDos').value;//Tomo datos por ID en la variable "Numero2"
+	NumeroUno = parseInt(NumeroUno); //Tranformo variable a un entero
+	NumeroDos = parseInt(NumeroDos); //Tranformo varable a un entero
+	
+	Resultado = NumeroUno + NumeroDos ;//Sumo Variables y almaceno en una variable
+	
+	alert("La suma es " + Resultado);//Muestro el resultado por alert 
 }
 
