481d03f1b8d54626e32b6bf59c3f9baf344c0172
federicogarcik <federico82008@gmail.com>
2017-02-07 12:05:02 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,29 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+	var Dividendo;
+	var divisor;
+	var Resto;
+	var Resultado;
+	/*
+	Defino las variables que voy a utlizar:
+	Dividendo: Numero que es dividido
+	Divisor: numero que divide
+	Resto: numero sobrante de las division
+	Resultado: Resultado de la division
+	*/
+
+	Dividendo = document.getElementById('numeroDividendo').value;
+	Divisor = document.getElementById('numeroDivisor').value;
+	//Ingreso  valor en las variables
+
+	Dividendo = parseInt(Dividendo);
+	Divisor = parseInt(Divisor);
+	//Tranformo las variables a valores enteros
+
+	Resultado =  Dividendo % Divisor;
+	//Divido la variable Dividendo por la variable Divisor
+
+	alert("El resto es " + Resultado);
+	//Muestro el resot por alert
 }
