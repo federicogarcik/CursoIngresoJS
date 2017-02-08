241220138f8f824794272dc0c9a6f92750fd822f
federicogarcik <federico82008@gmail.com>
2017-02-08 12:17:13 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,24 @@
 function Mostrar()
 {
 //tomo la edad  
+    var Edad;
 
+    Edad = document.getElementById('edad').value;
+
+    if(Edad < 18 && Edad > 12)
+	{
+
+	}
+	else
+	{
+		alert("Usted no es adolecente");
+	}
+
+/*
+    if(!(Edad < 18 && Edad > 12))
+{
+	alert("Usted no es adolecente");
+}
+*/   
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,28 @@
 function Mostrar()
 {
 //tomo la edad  
+    var Edad;
 
+    Edad = document.getElementById('edad').value;
 
+    if(Edad > 17)
+    {
+    	alert("Usted es un adulto");
+    }
+    else
+    {
+    	if (Edad < 13)
+    	{
+    		alert("Usted es un niño")
+    	}
+    	else
+    	{
+    		alert("Usted es un adolecente")
+    	}
+    }
+/*
+se puede hacer con 3 if's probrar
+*/
 
 
 }//FIN DE LA FUNCIÓN
