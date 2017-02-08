3fb437f215b44b9c739d8bed424c1bf1941f84e1
federicogarcik <federico82008@gmail.com>
2017-02-08 11:39:52 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,17 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var Importe;
+	var Resultado;
+	//Defino variables
+
+	Importe = document.getElementById('importe').value;
+	//Tomo el importe
+
+	Importe = parseInt(Importe);
+	//Tranformo Importe a entero
+
+	Resultado = Importe * 0.75;
+	document.getElementById('resultado').value = Resultado;
+	//Calculo descuento y lo muestro
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,11 @@ en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
 
-	
+	var Sueldo;
+	var Resultado;
+
+	Sueldo = document.getElementById('sueldo').value;
+	Sueldo = parseInt(Sueldo);
+	Resultado = Sueldo * 1.1;
+	document.getElementById('resultado').value = Resultado;
 }
