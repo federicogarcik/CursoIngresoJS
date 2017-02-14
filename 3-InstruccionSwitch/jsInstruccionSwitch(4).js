b5278503d9d812b415ff1204287ec6dd296c56f8
federicogarcik <federico82008@gmail.com>
2017-02-14 12:06:03 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,8 +4,27 @@ function Mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
-	
+switch(mesDelAño)
+{
+	case "Enero":
+	case "Marzo":  
+	case "Mayo":    
+	case "Julio":
+	case "Agosto":
+	case "Octubre":
+	case "Dicimebre":
+	    alert("Este mes tiene 31 dias");
+	    break           
+	case "Abril":
+	case "Junio":
+	case "Septiembre":
+	case "Noviembre":
+	    alert("Este mes tiene 30 dias");
+	    break
+	case "Febrero":
+	    alert("Este mes tiene 28 dias");
+	    break       
+}	
 
 
 
