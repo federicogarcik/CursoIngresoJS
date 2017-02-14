3bb479d900018f89048244e1f841816d6313ee51
federicogarcik <federico82008@gmail.com>
2017-02-14 09:51:25 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,23 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var NumeroRandom;
+	NumeroRandom = Math.floor(Math.random() * 10) + 1;
+	alert(NumeroRandom);
 
+	if(NumeroRandom > 8)
+	{
+		alert("EXCELENTE");
+	}
+	else
+	{
+		if(NumeroRandom > 4 && NumeroRandom < 9)
+	    {
+		    alert("APROBADO");
+	    }
+	    else
+	    {
+		    alert("Vamos, la proxima se puede");
+	    }
+    }
 }//FIN DE LA FUNCIÓN
