a76c96284330972be1baaf6c9de8b50a275c39e0
federicogarcik <federico82008@gmail.com>
2017-02-14 11:14:45 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -5,7 +5,36 @@ var mesDelAño = document.getElementById('mes').value;
 
 alert (mesDelAño);
 
-
-
-
-}//FIN DE LA FUNCIÓN
+if(mesDelAño == "Agosto" || mesDelAño == "Julio")
+{
+	alert("Abrigate que hace frio");
+}
+else
+{
+	if(mesDelAño != "Julio" || mesDelAño != "Agosto")
+    {
+	    switch(mesDelAño)
+	    {
+		    case "Enero":
+			    alert("Falta para el invierno");
+			    break	    
+		    case "Febrero":
+			    alert("Falta para el invierno");
+			    break
+		    case "Marzo":
+			    alert("Falta para el invierno");
+			    break
+		    case "Abril":
+			    alert("Falta para el invierno");
+			    break
+		    case "Mayo":
+			    alert("Falta para el invierno");
+			    break
+		    case "Junio":
+			    alert("Falta para el invierno");      
+			default:
+			alert("ya pasamos el frio , ahora el calor")
+		}	    		             
+	}	
+}
+}//FIN DE LA FUNCIÓN*/
