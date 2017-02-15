bbeca681f3d3ede0b9e521248562f98f9da7a834
federicogarcik <federico82008@gmail.com>
2017-02-14 21:07:09 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,7 +2,16 @@ function Mostrar()
 {
 //tomo la edad  
 
-	
+	var Edad;
+	var EstadoCivil;
+	EstadoCivil = document.getElementById('estadoCivil').value;
+	Edad = document.getElementById('edad').value;
+
+	if(Edad < 18 && EstadoCivil != "Soltero")
+	{
+		alert("Usted es muy pequeño para no ser soltero");
+	}
+
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,7 +2,15 @@ function Mostrar()
 {
 //tomo la edad  
 
-	
+	var Edad;
+	var EstadoCivil;
+	EstadoCivil = document.getElementById('estadoCivil').value;
+	Edad = document.getElementById('edad').value;
+
+	if(Edad > 17 && EstadoCivil == "Soltero")
+	{
+		alert("Es soltero y no es menor de edad");
+	}
 
 
 }//FIN DE LA FUNCIÓN
