e00f3344d05fece74396a8fb03a52d7c60df500c
federicogarcik <federico82008@gmail.com>
2017-02-22 09:26:03 -0300

    Add files via upload

--- 8-TPs/jsFonteCristo.js
+++ 8-TPs/jsFonteCristo.js
@@ -7,7 +7,33 @@ c.	Se pedirán un número positivo y se mostrará la cantidad de números divisi
 d.	Se pedirán un número positivo y se mostrará si el número es un número primo o no.
 e.	Se pedirán un número positivo y se mostrará la cantidad de números Primos desde el número ingresado hasta el cero.
 */
-function ComenzarIngreso () 
+
+function NumerosPares() 
 {
- 	
+	
+
+}
+
+function NumerosImpares() //Function numeros impares
+{
+	
+
+}
+
+function NumerosDivisibles()
+{
+	
+
+}
+
+function VerificarPrimo()
+{
+
+	
+}
+
+function NumerosPrimos()
+{
+
+
 }
